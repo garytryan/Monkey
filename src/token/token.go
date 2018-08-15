6b29a3f2d67b0424f618fc7d3ab4de9f3fc590b3
Gary Ryan <gary.thomas.ryan@gmail.com>
2018-08-14 22:07:58 -0700

    Monkey: Lexer - tokens

---
+++ src/token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// IDENTIFIERS + LITERALS
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// OPERATORS
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// DELIMITERS
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// KEYWORDS
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
